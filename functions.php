2dbe9e14eb46e7e4534d0e7cd8e9e689f58cd0b9
Tim Normington <tim.j.normington@gmail.com>
2017-03-20 01:53:51 -0700

    Added sketchbook custom post type

--- functions.php
+++ functions.php
@@ -68,57 +68,6 @@ class StarterSite extends TimberSite {
 	}
 
 
-	// Fix menu link highlighting
-
-
-  
-	
-	// function add_current_nav_class($classes, $item, $context) {
-		
-	// 	// Getting the current post details
-	// 	// global $post;
-		
-	// 	// Getting the post type of the current post
-	// 	// $current_post_type = get_post_type_object(get_post_type($post->ID));
-	// 	$current_post_type_slug = $context['post']->type;
-	// 	// $current_post_type_slug = $current_post_type->rewrite['slug'];
-
-	// 	// Getting the URL of the menu item
-	// 	$menu_slug = strtolower(trim($item->url));
-		
-	// 	// If the menu item URL contains the current post types slug add the current-menu-item class
-	// 	if (strpos($menu_slug,$current_post_type_slug) !== false) {
-		
-	// 	$classes[] = 'current-menu-item';
-		
-	// 	} else {
-			
-	// 		$classes = array_diff( $classes, array( 'current_page_parent' ) );
-	// 	}
-		
-	// 	// Return the corrected set of classes to be added to the menu item
-	// 	return $classes;
-
-	
-	// }
-
-
-
-	function custom_menu_item_classes($classes = array(), $menu_item = false){
- 
-		// use this format for removing highlighting
-		if((is_singular('project') || is_post_type_archive('projects')) && $menu_item->ID == 247) {
-			$classes = array();
-		}
-		
-		// use this format for adding highlighting
-		if((is_singular('project') || is_post_type_archive('projects')) && $menu_item->ID == 241) {
-			$classes[] = 'active';
-		}
-		
-		return $classes;
-	}
-	
 
 
 	function enqueue_resources() {
@@ -132,6 +81,7 @@ class StarterSite extends TimberSite {
 	function register_post_types() {
 		//this is where you can register custom post types
 		include('functions/project.cpt.php');
+		include('functions/sketchbook.cpt.php');
 	}
 
 	function register_taxonomies() {
