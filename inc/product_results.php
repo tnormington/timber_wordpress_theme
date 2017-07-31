a0fee62b3487933658cb0bf5f0f705ecb823b24c
Tim Normington <tim.j.normington@gmail.com>
2017-07-30 19:16:03 -0700

    Updated product placeholder image, and added UL

--- inc/product_results.php
+++ inc/product_results.php
@@ -1,4 +1,10 @@
 <div class="product-results">
     <h3 class="center">You Will Recieve</h3>
+    <ul>
+        <li>1 png portrait w/ colored circle background</li>
+        <li>1 png portrait w/ colored square background</li>
+        <li>1 png emoji</li>
+        <li>1 svg file of your face</li>
+    </ul>
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/portrait_placeholder.png">
 </div>
